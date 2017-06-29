50b19de30d0d4a19e5dcf99fb1f83d21323affcf
darthice <rithpansanga@gmail.com>
2017-06-29 18:02:52 +0700

    complete table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,20 @@
-class PagesController <ApplicationController
+class PagesController < ApplicationController
 
-	def main
+  def main
+  end
 
+  def home
+    @Teams = Team.all
+    @Homes = Home.all
+    @Aways = Away.all
+    @Matches = Match.all
+  end
 
-	end
-
-	def home
-		
-		@Teams = Team.all
-		@Homes = Home.all
-		@Aways = Away.all
-		@Matches = Match.all
-
-	end
-
-
-	def dark
-
-
-		
-		@scoretables =Scoretable.all
-
-		@matches = Match.all
-		@teams = Team.all
-		@matchcount = Match.where(home_id:[:id]).count 
-
-	end
-
+  def dark
+    @scoretables =Scoretable.all
+    @matches = Match.all
+    @teams = Team.select("teams.id, teams.name, SUM(scoretables.point) AS total_points").joins(:scoretables).group("teams.id").order("total_points DESC")
+    @matchcount = Match.where(home_id:[:id]).count 
+  end
 
 end
